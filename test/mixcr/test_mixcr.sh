804d185ce4f8fcb36b25871e2c4ba5805dcc186e
Brian Corrie <bdcorrie@gmail.com>
2019-01-23 21:20:39 +0000

    CHanged it so the MiXCR file is a gzipped file.

--- test/mixcr/test_mixcr.sh
+++ test/mixcr/test_mixcr.sh
@@ -10,4 +10,4 @@ then
 fi
 
 python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt -s -l $PWD -f PRJNA330606_Wang_One_Sample.csv
-python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt -m -l $PWD -f SRR4084215_aa_mixcr_annotation_1000_lines.txt
+python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt -m -l $PWD -f SRR4084215_aa_mixcr_annotation_1000_lines.txt.gz
