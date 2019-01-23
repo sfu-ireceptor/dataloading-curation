775d8a12f9d6b90d3b631755845e3ef124e5ef39
Brian Corrie <bdcorrie@gmail.com>
2019-01-23 20:29:39 +0000

    Cleaned up the test data set to be a documentes subset of a study, with real metadata.

--- test/mixcr/test_mixcr.sh
+++ test/mixcr/test_mixcr.sh
@@ -9,5 +9,5 @@ then
     export CONFIGPATH=../../../config
 fi
 
-python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt -s -l $PWD -f SRR1033679_mixcr_sample.csv
-python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt -m -l $PWD -f SRR1033679_mixcr_annotation_test.1000lines.txt
+python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt -s -l $PWD -f PRJNA330606_Wang_One_Sample.csv
+python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt -m -l $PWD -f SRR4084215_aa_mixcr_annotation_1000_lines.txt
