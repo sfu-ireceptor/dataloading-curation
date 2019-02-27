78f244fa7c9c5b996b6739fd8a1d166939c491b2
Brian Corrie <bdcorrie@gmail.com>
2019-02-27 22:17:12 +0000

    Made the output for the data loading verbose.

--- test/imgt/test_imgt_toy.sh
+++ test/imgt/test_imgt_toy.sh
@@ -14,4 +14,4 @@ then
 fi
 
 python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt --host=$DB_HOST -s -f imgt_toy/PRJNA248411_Palanichamy_SRR1298740.csv
-python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt --host=$DB_HOST -i -f imgt_toy/SRR1298740.txz
+python $PYTHONPATH/dataloader.py --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt --host=$DB_HOST -v -i -f imgt_toy/SRR1298740.txz
