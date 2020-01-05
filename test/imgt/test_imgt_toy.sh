0fa971e1816ae70b980af22791795aec1b8651ee
Brian Corrie <bdcorrie@gmail.com>
2020-01-05 01:25:03 +0000

    Changed so handles error codes correctly. Stops processing after an error.

--- test/imgt/test_imgt_toy.sh
+++ test/imgt/test_imgt_toy.sh
@@ -14,4 +14,7 @@ then
 fi
 
 python $PYTHONPATH/dataloader.py --database_map=ir_repository --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt --host=$DB_HOST -s -f imgt_toy/PRJNA248411_Palanichamy_SRR1298740.csv
-python $PYTHONPATH/dataloader.py --database_map=ir_repository --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt --host=$DB_HOST -v -i -f imgt_toy/SRR1298740.txz
+if [ $? -eq 0 ]
+then
+    python $PYTHONPATH/dataloader.py --database_map=ir_repository --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt --host=$DB_HOST -v -i -f imgt_toy/SRR1298740.txz
+fi
