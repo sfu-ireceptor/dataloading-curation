52406b32ea3281ae7e5a65432a3b4afe83ff2be5
Brian Corrie <bdcorrie@gmail.com>
2020-01-17 23:41:45 +0000

    Changes for new mapping and AIRR standard.

--- test/igblast/test_igblast.sh
+++ test/igblast/test_igblast.sh
@@ -17,5 +17,5 @@ fi
 python $PYTHONPATH/dataloader.py -v --ireceptor --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt --host=$DB_HOST -f analy13_sample.csv
 if [ $? -eq 0 ]
 then
-    python $PYTHONPATH/dataloader.py -v --airr --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt --host=$DB_HOST -f analy13.igblast_airr_annots.txt.gz
+    python $PYTHONPATH/dataloader.py -v --airr --mapfile=$CONFIGPATH/AIRR-iReceptorMapping.txt --host=$DB_HOST -f analy13.igblast_airr_annots.txt
 fi
