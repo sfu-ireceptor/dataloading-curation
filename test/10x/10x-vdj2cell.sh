d7c11085b7c12ab879e598635c006642520e4705
Brian Corrie <bdcorrie@gmail.com>
2022-04-08 15:58:43 -0700

    Fix off by one bug
    Script was dropping the first cell in the file.

--- test/10x/10x-vdj2cell.sh
+++ test/10x/10x-vdj2cell.sh
@@ -15,4 +15,4 @@ fi
 #     - We want a line per cell, with cell_id = barcode
 #     - We have extra stuff per line (e.g. virtual_pairing) that is constant
 #     - We need to separate with commas except on the last line (the NR stuff)
-cat $1 | grep "\"" | awk -F'"' '{print $2}' | sort -u | awk -F'\t' 'BEGIN {therest=",\"virtual_pairing\":false, \"expression_study_method\":\"single-cell transcriptome\""; print "["} {if (NR>1) { printf("{\"cell_id\":\"%s\"%s},\n",$1,therest); } str=$1; } END {printf("{\"cell_id\":\"%s\"%s}\n",str,therest); print "]"}'
+cat $1 | grep "\"" | awk -F'"' '{print $2}' | sort -u | awk -F'\t' 'BEGIN {therest=",\"virtual_pairing\":false, \"expression_study_method\":\"single-cell transcriptome\""; print "["} {if (NR>0) { printf("{\"cell_id\":\"%s\"%s},\n",$1,therest); } str=$1; } END {printf("{\"cell_id\":\"%s\"%s}\n",str,therest); print "]"}'
